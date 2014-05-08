61709d8eeddcd06ab747abc2a264fbde1b48744c
John.Koepi <john.koepi@gmail.com>
2014-05-08 21:00:02 +0300

    Quad tree impl to store adjacent vm pages

--- qtree.go
+++ qtree.go
@@ -3,6 +3,20 @@ Based on work of Volker Poplawski, 2013 (https://github.com/volkerp/goquadtree)
 */
 package main
 
+// Number of entries until a quad is split
+const MAX_ENTRIES_PER_TILE = 16
+
+// Maximum depth of quad-tree (not counting the root node)
+const MAX_LEVELS = 64
+
+// AABB orientation inside tree
+const (
+	_TOPRIGHT    = 0
+	_TOPLEFT     = 1
+	_BOTTOMLEFT  = 2
+	_BOTTOMRIGHT = 3
+)
+
 // Use AABB() to construct a AABB object
 type AABB struct {
 	MinX, MaxX, MinY, MaxY int64
@@ -51,4 +65,127 @@ func (b AABB) Intersects(o AABB) bool {
 func (b AABB) Contains(o AABB) bool {
 	return b.MinX <= o.MinX && b.MinY <= o.MinY &&
 		b.MaxX >= o.MaxX && b.MaxY >= o.MaxY
+}
+
+
+// QuadTree expects its values to implement the AABBi interface.
+type QuadElement interface {
+	AABB() AABB
+}
+
+// quad-tile / node of the quad-tree
+type QuadTile struct {
+	AABB
+
+	level    int           // level this tile is at. root is level 0
+	contents []QuadElement // values stored in this tile
+	childs   [4]*QuadTile  // sub-tiles. none or four.
+}
+
+type QuadTree struct {
+	root QuadTile
+}
+
+// Constructs an empty quad-tree
+// bbox specifies the extends of the coordinate system.
+func NewQuadTree(bbox AABB) QuadTree {
+	qt := QuadTree{ QuadTile{AABB:bbox} }
+	return qt
+}
+
+// Adds a value to the quad-tree by trickle down from the root node.
+func (qb *QuadTree) Add(v QuadElement) {
+	qb.root.add(v)
+}
+
+// Returns all values which intersect the query box
+func (qb *QuadTree) Query(bbox AABB) (values []QuadElement) {
+	return qb.root.query(bbox, values)
+}
+
+func (tile *QuadTile) add(v QuadElement) {
+	// look for sub-tile directly below this tile to accomodate value.
+	if i := tile.findChildIndex(v.AABB()); i < 0 {
+		// no suitable sub-tile for value found.
+		// either this tile has no childs or
+		// value does not fit in any subtile.
+		// store value at this level.
+		tile.contents = append(tile.contents, v)
+
+		// tile is split if exceeds it max number of entries and
+		// has not childs already and max tree depth for this sub-tree not reached.
+		if len(tile.contents) > MAX_ENTRIES_PER_TILE && tile.childs[_TOPRIGHT] == nil && tile.level < MAX_LEVELS {
+			tile.split()
+		}
+	} else {
+		// suitable sub-tile for value found at index i.
+		// recursivly add value.
+		tile.childs[i].add(v)
+	}
+}
+
+
+// return child index for AABB
+// returns -1 if quad has no children or AABB does not fit into any child
+func (tile *QuadTile) findChildIndex(bbox AABB) int {
+	if tile.childs[_TOPRIGHT] == nil {
+		return -1
+	}
+
+	for i, child := range tile.childs {
+		if child.Contains(bbox) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+
+// create four child quads.
+// distribute contents of this tiles on newly created childs.
+func (tile *QuadTile) split() {
+	mx := tile.MaxX/2.0 + tile.MinX/2.0
+	my := tile.MaxY/2.0 + tile.MinY/2.0
+
+	tile.childs[_TOPRIGHT]    = &QuadTile{ AABB:NewAABB(mx, tile.MaxX, my, tile.MaxY), level:tile.level+1 }
+	tile.childs[_TOPLEFT]     = &QuadTile{ AABB:NewAABB(tile.MinX, mx, my, tile.MaxY), level:tile.level+1 }
+	tile.childs[_BOTTOMLEFT]  = &QuadTile{ AABB:NewAABB(tile.MinX, mx, tile.MinY, my), level:tile.level+1 }
+	tile.childs[_BOTTOMRIGHT] = &QuadTile{ AABB:NewAABB(mx, tile.MaxX, tile.MinY, my), level:tile.level+1 }
+
+	// copy values to temporary slice
+	var contentsBak []QuadElement
+	contentsBak = append(contentsBak, tile.contents...)
+
+	// clear values on this tile
+	tile.contents = []QuadElement{}
+
+	// reinsert from temporary slice
+	for _,v := range contentsBak {
+		tile.add(v)
+	}
+}
+
+
+func (tile *QuadTile) query(qbox AABB, ret []QuadElement) []QuadElement {
+	// end recursion if this tile does not intersect the query range
+	if ! tile.Intersects(qbox) {
+		return ret
+	}
+
+	// return candidates at this tile
+	for _, v := range tile.contents {
+		if qbox.Intersects(v.AABB()) {
+			ret = append(ret, v)
+		}
+	}
+
+	// recurse into childs (if any)
+	if tile.childs[_TOPRIGHT] != nil {
+		for _, child := range tile.childs {
+			ret = child.query(qbox, ret)
+		}
+	}
+
+	return ret
 }
--- qtree_test.go
+++ qtree_test.go
@@ -3,7 +3,9 @@ Based on work of Volker Poplawski, 2013 (https://github.com/volkerp/goquadtree)
 */
 package main
 
+import "math/rand"
 import "testing"
+import _ "fmt"
 
 func TestAABB(t *testing.T) {
 	a := NewAABB( 0, 10, 0, 10 )
@@ -64,3 +66,216 @@ func TestAABB(t *testing.T) {
 		t.Errorf("%v does not intersect %v", a, f)
 	}
 }
+
+// Generates n AABBes in the range of frame with average width and height avgSize
+func randomAABBes(n int, frame AABB, avgSize int64) []AABB {
+	ret := make([]AABB, n)
+
+	for i := 0; i < len(ret); i++ {
+		w := int64(rand.NormFloat64() * float64(avgSize))
+		h := int64(rand.NormFloat64() * float64(avgSize))
+		x := int64(rand.Float64() * float64(frame.SizeX()) + float64(frame.MinX))
+		y := int64(rand.Float64() * float64(frame.SizeY()) + float64(frame.MinY))
+		ret[i] = NewAABB(x, Min(frame.MaxX, x+w), y, Min(frame.MaxY, y+h))
+	}
+
+	return ret
+}
+
+
+// Returns all elements of data which intersect query
+func queryLinear(data []AABB, query AABB) (ret []QuadElement) {
+	for _, v := range data {
+		if query.Intersects(v.AABB()) {
+			ret = append(ret, v)
+		}
+	}
+
+	return ret
+}
+
+
+func compareQuadElement(v1, v2 QuadElement) bool {
+	b1 := v1.AABB()
+	b2 := v2.AABB()
+
+	return b1.MinX == b2.MinX && b1.MaxX == b2.MaxX &&
+		b1.MinY == b2.MinY && b2.MaxY == b2.MaxY
+}
+
+
+func lookupResults(r1, r2 []QuadElement) int {
+	for i, v1 := range r1 {
+		found := false
+
+		for _, v2 := range r2 {
+			if compareQuadElement(v1, v2) {
+				found = true
+				break
+			}
+		}
+
+		if ! found {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// World-space extends from -1000..1000 in X and Y direction
+var world AABB = NewAABB(-1000, 1000, -1000, 1000)
+
+
+// Compary correctness of quad-tree results vs simple look-up on set of random rectangles
+func TestQuadTreeRects(t *testing.T) {
+	var rects []AABB = randomAABBes(100, world, 5)
+	qt := NewQuadTree(world)
+
+	for _, v := range rects {
+		qt.Add(v)
+	}
+
+	queries := randomAABBes(100, world, 10)
+
+	for _, q := range queries {
+		r1 := queryLinear(rects, q)
+		r2 := qt.Query(q)
+
+		if len(r1) != len(r2) {
+			t.Errorf("r1 and r2 differ: %v   %v\n", r1, r2)
+		}
+
+		if i := lookupResults(r1, r2); i != -1 {
+			t.Errorf("%v was not returned by QT\n", r1[i])
+		}
+
+		if i := lookupResults(r2, r1); i != -1 {
+			t.Errorf("%v was not returned by brute-force\n", r2[i])
+		}
+
+	}
+}
+
+
+// Compary correctness of quad-tree results vs simple look-up on set of random points
+func TestQuadTreePoints(t *testing.T) {
+	var points []AABB = randomAABBes(100, world, 0)
+	qt := NewQuadTree(world)
+
+	for _, v := range points {
+		qt.Add(v)
+	}
+
+	queries := randomAABBes(100, world, 10)
+
+	for _, q := range queries {
+		r1 := queryLinear(points, q)
+		r2 := qt.Query(q)
+
+		if len(r1) != len(r2) {
+			t.Errorf("r1 and r2 differ: %v   %v\n", r1, r2)
+		}
+
+		if i := lookupResults(r1, r2); i != -1 {
+			t.Errorf("%v was not returned by QT\n", r1[i])
+		}
+
+		if i := lookupResults(r2, r1); i != -1 {
+			t.Errorf("%v was not returned by brute-force\n", r2[i])
+		}
+
+	}
+}
+
+// A set of 10 million randomly distributed rectangles of avg size 5
+var boxes10M []AABB
+
+func BenchmarkInitBoxes(b *testing.B) {
+	boxes10M = randomAABBes(10*1000*1000, world, 5)
+}
+
+// Benchmark insertion into quad-tree
+func BenchmarkInsert(b *testing.B) {
+	b.StopTimer()
+
+	var values []AABB = randomAABBes(b.N, world, 5)
+	qt := NewQuadTree(world)
+
+	b.StartTimer()
+
+	for _, v := range values {
+		qt.Add(v)
+	}
+}
+
+
+// Benchmark quad-tree on set of rectangles
+func BenchmarkRectsQuadtree(b *testing.B) {
+	b.StopTimer()
+	rand.Seed(1)
+	qt := NewQuadTree(world)
+
+	for _, v := range boxes10M {
+		qt.Add(v)
+	}
+
+	queries := randomAABBes(b.N, world, 10)
+
+	b.StartTimer()
+	for _, q := range queries {
+		qt.Query(q)
+	}
+}
+
+
+// Benchmark simple look up on set of rectangles
+func BenchmarkRectsLinear(b *testing.B) {
+	b.StopTimer()
+	rand.Seed(1)
+	queries := randomAABBes(b.N, world, 10)
+
+	b.StartTimer()
+	for _, q := range queries {
+		queryLinear(boxes10M, q)
+	}
+}
+
+// A set of 10 million randomly distributed points
+var points10M []AABB
+
+func BenchmarkInitPoints(b *testing.B) {
+	points10M = randomAABBes(10*1000*1000, world, 0)
+}
+
+// Benchmark quad-tree on set of points
+func BenchmarkPointsQuadtree(b *testing.B) {
+	b.StopTimer()
+	rand.Seed(1)
+	qt := NewQuadTree(world)
+
+	for _, v := range points10M {
+		qt.Add(v)
+	}
+
+	queries := randomAABBes(b.N, world, 10)
+
+	b.StartTimer()
+	for _, q := range queries {
+		qt.Query(q)
+	}
+}
+
+
+// Benchmark simple look-up on set of points
+func BenchmarkPointsLinear(b *testing.B) {
+	b.StopTimer()
+	rand.Seed(1)
+	queries := randomAABBes(b.N, world, 10)
+
+	b.StartTimer()
+	for _, q := range queries {
+		queryLinear(points10M, q)
+	}
+}
+
