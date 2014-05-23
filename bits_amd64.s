cebbd837d98ef718c152b50d53ef57a351c0c079
John.Koepi <john.koepi@gmail.com>
2014-05-23 17:08:21 +0300

    popcnt reuse defs

--- bits_amd64.s
+++ bits_amd64.s
@@ -1,15 +1,14 @@
 // #include "${GOROOT}/cmd/ld/textflag.h"
 // Don't insert stack check preamble.
-// #define NOSPLIT	4
 
+#define NOSPLIT 4
 #define POPCNT_AX_AX BYTE $0xf3; BYTE $0x48; BYTE $0x0f; BYTE $0xb8; BYTE $0xc0
 
 // func PopCnt(n uint64) uint64
 // void PopCnt(unsigned long long *ret, unsigned long long n);
-// TEXT ·PopCnt(SB),NOSPLIT,$0
-TEXT ·PopCnt(SB),4,$0-8
+// TEXT ·PopCnt(SB),NOSPLIT,$0-8
+TEXT ·PopCnt(SB),NOSPLIT,$0-8
     MOVQ n+0(FP), AX
-//  POPCNT AX, AX
-    BYTE $0xf3; BYTE $0x48; BYTE $0x0f; BYTE $0xb8; BYTE $0xc0
+    POPCNT_AX_AX
     MOVQ AX, ret+8(FP)
     RET
