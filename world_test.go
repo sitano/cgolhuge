cc7e3aca6b1e13f9625f000e46829b0ac6a821e6
sitano <john.koepi@gmail.com>
2014-05-26 09:15:26 +0300

    step bench

--- world_test.go
+++ world_test.go
@@ -1,4 +1,16 @@
 package main
+
+import "testing"
+
+func BenchmarkWorldGliderGun(t *testing.B) {
+	w := NewLifeWorld(NewWorldView(NewVM()))
+	LoadLIF(w, 20, 10, "pattern/glider_gun.lif")
+	t.ResetTimer()
+	for i := 0; i < t.N; i ++ {
+		w.Step()
+	}
+}
+
 /*
 import "testing"
 import "math"
@@ -123,194 +135,4 @@ func TestWorldStep3(t *testing.T) {
 	if w.Get(math.MinInt64, math.MaxInt64, w.Layer()) != LIFE { t.Error("No life at-1, 0") }
 }
 
-func printGliderSE(w *LifeWorld, x int64, y int64) {
-	w.Set(x, y, w.Layer(), LIFE)
-	w.Set(x + 1, y, w.Layer(), LIFE)
-	w.Set(x + 2, y, w.Layer(), LIFE)
-	w.Set(x + 2, y + 1, w.Layer(), LIFE)
-	w.Set(x + 1, y + 2, w.Layer(), LIFE)
-}
-
-func printGliderSW(w *LifeWorld, x int64, y int64) {
-	w.Set(x, y, w.Layer(), LIFE)
-	w.Set(x + 1, y, w.Layer(), LIFE)
-	w.Set(x + 2, y, w.Layer(), LIFE)
-	w.Set(x, y + 1, w.Layer(), LIFE)
-	w.Set(x + 1, y + 2, w.Layer(), LIFE)
-}
-
-func printGliderNE(w *LifeWorld, x int64, y int64) {
-	w.Set(x+2, y, w.Layer(), LIFE)
-	w.Set(x+2, y + 1, w.Layer(), LIFE)
-	w.Set(x+2, y + 2, w.Layer(), LIFE)
-	w.Set(x+1, y + 2, w.Layer(), LIFE)
-	w.Set(x, y + 1, w.Layer(), LIFE)
-}
-
-func printGliderNW(w *LifeWorld, x int64, y int64) {
-	w.Set(x, y, w.Layer(), LIFE)
-	w.Set(x, y + 1, w.Layer(), LIFE)
-	w.Set(x, y + 2, w.Layer(), LIFE)
-	w.Set(x + 1, y + 2, w.Layer(), LIFE)
-	w.Set(x + 2, y + 1, w.Layer(), LIFE)
-}
-
-func TestWorldStep4(t *testing.T) {
-	w := NewLifeWorldXY(NewAABBMax())
-	printGliderSE(&w, 3, 3)
-	printGliderSW(&w, 10, 10)
-	printGliderNW(&w, 3, 10)
-	printGliderNE(&w, 10, 3)
-	//printPages(&w)
-	//fmt.Print("Generation ", w.Generation(), "\n", printWorld(&w, NewAABB(16, 20, 16, 20)))
-	if w.v.vm.Reserved() != 1 {
-		printPages(&w)
-		t.Error("Why not 1???")
-	}
-	for i := 0 ; i < 100 ; i ++ {
-		w.Step()
-
-		//printPages(&w)
-		//fmt.Print("Generation ", w.Generation(), "\n", printWorld(&w, NewAABB(-16, 20, -16, 20)))
-		// fmt.Print(printHeatMap(&w, NewAABB(0, 16, 0, 8)))
-	}
-	//fmt.Print("Generation ", w.Generation(), "\n", printWorld(&w, NewAABB(0, 16, 0, -8)))
-	if w.v.vm.Reserved() != 4 {
-		printPages(&w)
-		t.Error("Why not 3??? ", w.v.vm.Reserved())
-	}
-}
-
-func BenchmarkWorldStep2pages(b *testing.B) {
-	w := NewLifeWorldXY(NewAABBMax())
-	w.Set(1, 0, w.Layer(), LIFE)
-	w.Set(0, 0, w.Layer(), LIFE)
-	w.Set(-1, 0, w.Layer(), LIFE)
-
-	b.ResetTimer()
-
-	for i := 0 ; i < b.N ; i ++ {
-		w.Step()
-	}
-}
-
-func BenchmarkWorldStep1pages(b *testing.B) {
-	b.StopTimer()
-	w := NewLifeWorldXY(NewAABBMax())
-	//w.Set(1, 0, w.Layer(), LIFE)
-	w.Set(0, 0, w.Layer(), LIFE)
-	w.Set(1, 0, w.Layer(), LIFE)
-	w.Set(0, 1, w.Layer(), LIFE)
-	w.Set(1, 1, w.Layer(), LIFE)
-	//w.Set(-1, 0, w.Layer(), LIFE)
-
-	b.StartTimer()
-
-	for i := 0 ; i < b.N; i ++ {
-		w.Step()
-	}
-}
-
-func BenchmarkWorldReadPage2(b *testing.B) {
-	b.StopTimer()
-	w := NewLifeWorldXY(NewAABBMax())
-	//w.Set(1, 0, w.Layer(), LIFE)
-	w.Set(0, 0, w.Layer(), LIFE)
-	w.Set(1, 0, w.Layer(), LIFE)
-	w.Set(0, 1, w.Layer(), LIFE)
-	w.Set(1, 1, w.Layer(), LIFE)
-	w.Set(-1, 0, w.Layer(), LIFE)
-
-	lw := &w
-
-	b.StartTimer()
-
-	for i := 0 ; i < b.N; i ++ {
-		lw.generation ++
-		lw.population = 0
-		cz := lw.z
-		//nz := lw.NextZLayer()
-		//ws := lw.v.pb.wsize
-		ks := lw.v.vm.ksize
-
-		// Process life
-		//new := list.New()
-		lw.population = lw.v.pb.Reduce(func(a interface{}, pt *PageTile) interface{} {
-				population := a.(uint64)
-
-				// Prevent reclamation during processing
-				pt.alive = 1
-
-				for i := uint(0) ; i < ks; i ++ {
-					//x := POtoWX(i, pt.px, ws)
-					//y := POtoWY(i, pt.py, ws)
-
-					sum := DEAD // lw.LifeSumAt(x, y, cz)
-					st := lw.Get(0, 0, cz)
-					//nst := DEAD
-
-					if st == DEAD {
-						if sum == RULE_BORN {
-							//		nst = LIFE
-							pt.alive ++
-							population ++
-						}
-					}
-
-					if st == LIFE {
-						if sum >= RULE_LIVE_MIN && sum <= RULE_LIVE_MAX {
-							//		nst = LIFE
-							pt.alive ++
-							population ++
-						}
-					}
-				}
-
-				pt.alive --
-
-				return population
-			}, lw.population).(uint64)
-	}
-}
-
-
-func BenchmarkWorldRWPage(b *testing.B) {
-	b.StopTimer()
-	w := NewLifeWorldXY(NewAABBMax())
-	//w.Set(1, 0, w.Layer(), LIFE)
-	w.Set(0, 0, w.Layer(), LIFE)
-	w.Set(1, 0, w.Layer(), LIFE)
-	w.Set(0, 1, w.Layer(), LIFE)
-	w.Set(1, 1, w.Layer(), LIFE)
-
-	lw := &w
-
-	b.StartTimer()
-
-	for i := 0 ; i < b.N; i ++ {
-		lw.generation ++
-		lw.population = 0
-		cz := lw.z
-		nz := lw.NextZLayer()
-		ws := lw.v.pb.wsize
-		ks := lw.v.vm.ksize
-		lw.population = lw.v.pb.Reduce(func(a interface{}, pt *PageTile) interface{} {
-				population := a.(uint64)
-
-				// Prevent reclamation during processing
-				pt.alive = 1
-
-				for i := uint(0) ; i < ks; i ++ {
-					x := POtoWX(i, pt.px, ws)
-					y := POtoWY(i, pt.py, ws)
-					st := lw.Get(x, y, cz)
-					lw.Set(x, y, nz, st)
-				}
-
-				pt.alive --
-
-				return population
-			}, lw.population).(uint64)
-	}
-}
 */
