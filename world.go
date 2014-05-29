fac1a56a5e5ad5eed5e9e9524be07865eb596ba7
sitano <john.koepi@gmail.com>
2014-05-29 22:30:37 +0300

    opt page step middle bits proc - win 1/4 of throughput (5000ns win on mac book air)

--- world.go
+++ world.go
@@ -109,72 +109,80 @@ func (w *LifeWorld) Step() {
 
 			// Process 1 stride line if there are anything to process
 			if prev_line | curr_line | next_line != 0 {
-				// First 2 bits with mask 0b011
-				// Test: go build && ./cgolhuge -load pattern/glider_gun.lif -lx 17 -ly 5 -wait
-				sum := PopCnt((prev_line & BITS11) +
-					(curr_line & BITS10) << 4 +
-					(next_line & BITS11) << 8)
+				{
+					// First 2 bits with mask 0b011
+					// Test: go build && ./cgolhuge -load pattern/glider_gun.lif -lx 17 -ly 5 -wait
+					sum := PopCnt((prev_line & BITS11) +
+						(curr_line & BITS10) << 4 +
+						(next_line & BITS11) << 8)
 
-				if sum >= RULE_LIVE_MIN {
-					st := byte(curr_line & BITS1)
+					if sum >= RULE_LIVE_MIN {
+						st := byte(curr_line & BITS1)
 
-					if st == DEAD {
-						if sum == RULE_BORN {
-							new_line = new_line | BITS1
-							w.population ++
-						}
-					} else {
-						if sum >= RULE_LIVE_MIN && sum <= RULE_LIVE_MAX {
-							new_line = new_line | BITS1
-							w.population ++
-						}
-					}
-				}
-
-				// Middle bits
-				for bi := uint(1); bi < PageStrideBits - 1; bi ++ {
-					bi_1 := bi - 1
-					sum := PopCnt((prev_line & (BITS111 << bi_1)) >> bi_1 +
-							((curr_line & (BITS101 << bi_1)) >> bi_1) << 4 +
-							((next_line & (BITS111 << bi_1)) >> bi_1) << 8)
-
-					if sum < RULE_LIVE_MIN {
-						continue
-					}
-
-					st := byte(PopCnt(curr_line & (BITS1 << bi)))
-
-					if st == DEAD {
-						if sum == RULE_BORN {
-							new_line = new_line | (BITS1 << bi)
-							w.population ++
-						}
-					} else {
-						if sum >= RULE_LIVE_MIN && sum <= RULE_LIVE_MAX {
-							new_line = new_line | (BITS1 << bi)
-							w.population ++
+						if st == DEAD {
+							if sum == RULE_BORN {
+								new_line |= BITS1
+								w.population ++
+							}
+						} else {
+							if sum <= RULE_LIVE_MAX {
+								new_line |= BITS1
+								w.population ++
+							}
 						}
 					}
 				}
 
-				// Last 2 bits with mask 0b011
-				// Test: go build && ./cgolhuge -load pattern/glider_gun.lif -lx 45 -ly 5 -wait
-				sum = PopCnt(((prev_line >> PageStrideLast2) & BITS11) +
-					((curr_line >> PageStrideLast2) & BITS01) << 4 +
-					((next_line >> PageStrideLast2) & BITS11) << 8)
+				{
+					// Middle bits
+					pl := prev_line
+					cl := curr_line
+					nl := next_line
+					for bi := uint(1); bi < PageStrideBits - 1; bi ++ {
+						sum := PopCnt((pl & BITS111) + (cl & BITS101) << 4 + (nl & BITS111) << 8)
 
-				if sum >= RULE_LIVE_MIN {
-					st := byte(curr_line >> PageStrideLast1)
+						if sum >= RULE_LIVE_MIN {
+							st := byte((cl >> 1) & BITS1)
 
-					if st == DEAD {
-						if sum == RULE_BORN {
-							new_line = new_line | (BITS1 << PageStrideLast1)
-							w.population ++
+							if st == DEAD {
+								if sum == RULE_BORN {
+									new_line |= BITS1 << bi
+									w.population ++
+								}
+							} else {
+								if sum <= RULE_LIVE_MAX {
+									new_line |= BITS1 << bi
+									w.population ++
+								}
+							}
 						}
-					} else {
-						if sum >= RULE_LIVE_MIN && sum <= RULE_LIVE_MAX {
-							new_line = new_line | (BITS1 << PageStrideLast1)
-							w.population ++
+
+						pl >>= 1
+						cl >>= 1
+						nl >>= 1
+					}
+				}
+
+				{
+					// Last 2 bits with mask 0b011
+					// Test: go build && ./cgolhuge -load pattern/glider_gun.lif -lx 45 -ly 5 -wait
+					sum := PopCnt(((prev_line >> PageStrideLast2) & BITS11) +
+						((curr_line >> PageStrideLast2) & BITS01) << 4 +
+						((next_line >> PageStrideLast2) & BITS11) << 8)
+
+					if sum >= RULE_LIVE_MIN {
+						st := byte(curr_line >> PageStrideLast1)
+
+						if st == DEAD {
+							if sum == RULE_BORN {
+								new_line |= BITS1 << PageStrideLast1
+								w.population ++
+							}
+						} else {
+							if sum <= RULE_LIVE_MAX {
+								new_line |= BITS1 << PageStrideLast1
+								w.population ++
+							}
 						}
 					}
 				}
